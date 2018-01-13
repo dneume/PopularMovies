10f4f35cffe65703dd42b1275f5564ddf9e14f69
Dave Neumeister <dneume@gmail.com>
2018-01-13 08:35:29 -0500

    Added code to:     1. build the url     2. query the movie db site     3. configure the AsycTask
    Signed-off-by: Dave Neumeister <dneume@gmail.com>

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,29 +1,74 @@
 package com.example.android.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.URL;
+
+import static com.example.android.popularmovies.Utils.getResponseFromHttpUrl;
 import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 
 
 public class MainActivity extends AppCompatActivity implements ImageAdapter.ItemClickListener {
-
     private ImageAdapter adapter;
+
     private Boolean networkIsAvailable = FALSE;
     private Context context;
+    public TextView mSearchResultsTextView;
+
+
+    public class MovieDbQueryTask extends AsyncTask<URL, Void, String> {
+
+        //http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=78891bb3d2c1b1ee69109f6f46b23ead
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String movieDbResults = null;
+
+            try {
+                movieDbResults = getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+              return movieDbResults;
+        }
+
+        @Override
+        protected void onPostExecute(String movieDbResults) {
+            Log.d("onPostExecute",movieDbResults);
+            showJsonDataView();
+            Log.d("Post show JsonData","Post show JsonData");
+            mSearchResultsTextView.setText(movieDbResults);
+            Log.d("onPost show JsonData","getting ready to exit");
+        }
+     }
+
+    private void showJsonDataView() {
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+    private void hideJsonDataView() {
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         int numberOfColumns = 0;
-
+        URL url;
         Utils utils;
         RecyclerView.Adapter recyclerView_Adapter;
         RecyclerView.LayoutManager recyclerView_LayoutManager;
@@ -40,23 +85,34 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Item
         numberOfColumns = 2;
         context = getApplicationContext();
 
+        //hide the extra text view until it is needed
+        mSearchResultsTextView = findViewById(R.id.r_error);
+        hideJsonDataView();
 
         // is the network available
         utils = new Utils();
         networkIsAvailable = utils.isNetworkAvailable(context);
 
+        // set up the recycler view with the layout and the adapter
         recyclerView = findViewById(R.id.rvImages);
         recyclerView_LayoutManager = new GridLayoutManager(context, numberOfColumns);
         if (recyclerView_LayoutManager != null) {
             recyclerView.setLayoutManager(recyclerView_LayoutManager);
         }
 
+        url = Utils.buildUrl();
+
+        //Execute the MovieDbQuery by instantiating the MovieDbQueryTask
+        Log.d("1 process",url.toString());
+        new MovieDbQueryTask().execute(url);
+        Log.d("2 process",url.toString());
+
         recyclerView_Adapter = new ImageAdapter(context, data);
         if (recyclerView_Adapter != null) {
             recyclerView.setAdapter(recyclerView_Adapter);
         }
-
     }
+
     @Override
     public void onItemClick(View view, int position) {
 
@@ -71,6 +127,7 @@ public class MainActivity extends AppCompatActivity implements ImageAdapter.Item
             Toast.makeText(this, "Try again once a network is available!", Toast.LENGTH_LONG).show();
             finish();
         }
+
     }
 
 }
--- app/src/main/java/com/example/android/popularmovies/Utils.java
+++ app/src/main/java/com/example/android/popularmovies/Utils.java
@@ -8,11 +8,29 @@ import static android.content.Context.CONNECTIVITY_SERVICE;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
+import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.IOException;
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
 /**
  * Created by dneum on 1/11/2018.
  */
 
 public class Utils {
+
+    final static String BASE_URL =
+            "http://api.themoviedb.org/3/discover/movie/";
+    final static String KEY_ID1 = "sort_by";
+    final static String PARAM_POPULAR = "popularity.desc";
+    final static String KEY_ID2 = "api_key";
+    final static String PARAM_API_KEY = "78891bb3d2c1b1ee69109f6f46b23ead";
+
 
     public void Utils() {
 
@@ -33,4 +51,49 @@ public class Utils {
         }
 
     }
+
+
+    public static URL buildUrl(){
+        URL url = null;
+        Uri.Builder uriBuilder = null;
+        Uri uri;
+
+        uri = Uri.parse(BASE_URL);
+        uriBuilder = uri.buildUpon();
+        uriBuilder.appendQueryParameter(KEY_ID1, PARAM_POPULAR);
+        uriBuilder.appendQueryParameter(KEY_ID2, PARAM_API_KEY);
+        uri = uriBuilder.build();
+
+        String local = uriBuilder.toString();
+
+        try{
+            url = new URL(uri.toString());
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.d("build url", local);
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
+
